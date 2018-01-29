f783b6a3378541cc1fe75aa2398f824239bd6c70
Elias Ojala <ojala.s.elias@gmail.com>
2018-01-29 19:40:57 +0200

    First version of theel0ja.js

---
+++ dist/index.js
@@ -0,0 +1,15 @@
+/* theel0ja.js */
+
+var theel0ja = {
+
+  /* https://stackoverflow.com/a/901144/6451184 */
+  getParameterByName: function(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)");
+    var results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+  }
+};
